6de393aa800acebd8bbd4a490d6c321a8e1475fe
Rustle Karl <fu.jiawei@outlook.com>
2022-05-16 19:34:14 +0800

    update: blacklist

--- github_trending_migrator/blacklist.py
+++ github_trending_migrator/blacklist.py
@@ -2,7 +2,7 @@
 Date: 2022.05.15 23:25
 Description: Omit
 LastEditors: Rustle Karl
-LastEditTime: 2022.05.16 07:45:15
+LastEditTime: 2022.05.16 19:06:53
 """
 
 repos = set(
@@ -11,5 +11,8 @@ https://github.com/zbezj/HEU_KMS_Activator
 https://github.com/microsoft/PowerToys
 https://github.com/vernesong/OpenClash
 https://github.com/CleverRaven/Cataclysm-DDA
+https://github.com/qgis/QGIS
+https://github.com/BabylonJS/Babylon.js
+https://github.com/ValveSoftware/source-sdk-2013
 """.splitlines()
 )

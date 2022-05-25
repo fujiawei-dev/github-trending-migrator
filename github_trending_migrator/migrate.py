927aac1b8b45440ea9d51176a7f01358f8964921
Rustle Karl <fu.jiawei@outlook.com>
2022-05-25 10:24:03 +0800

    add: 防止仓库重复

--- github_trending_migrator/migrate.py
+++ github_trending_migrator/migrate.py
@@ -2,27 +2,77 @@
 Date: 2022.05.09 20:20
 Description: Omit
 LastEditors: Rustle Karl
-LastEditTime: 2022.05.24 22:35:19
+LastEditTime: 2022.05.25 10:10:22
 """
 import asyncio
 from itertools import chain
+from typing import List
 
 import aiohttp
 
 from .blacklist import repos as repos_blacklist
 from .logger import log
-from .scraping import GITHUB_TRENDING_URL as base_url, get_github_trending
+from .scraping import GITHUB_TRENDING_URL, get_github_trending
 
 
-async def migrate_to_gitea(session, clone_addr, repo_name, description):
-    log.debug(clone_addr)
+async def get_all_organizations_list(
+    session: aiohttp.ClientSession,
+    base_url: str,
+    auth: aiohttp.BasicAuth,
+) -> List[str]:
+    response: aiohttp.ClientResponse = await session.get(
+        url=base_url + "/api/v1/admin/orgs",
+        auth=auth,
+    )
+
+    organizations = await response.json()
+
+    log.debug(f"Response [{response.status}]")
+    log.debug(f"Body {organizations}")
+
+    if response.status != 200:
+        return []
+
+    return [organization["username"] for organization in organizations]
+
+
+async def get_all_repositories_list(
+    session: aiohttp.ClientSession,
+    base_url: str,
+    auth: aiohttp.BasicAuth,
+    organization: str,
+):
+    response: aiohttp.ClientResponse = await session.get(
+        url=base_url + "/api/v1/orgs/" + organization + "/repos",
+        auth=auth,
+    )
+
+    repositories = await response.json()
+
+    log.debug(f"Response [{response.status}]")
+    log.debug(f"Body {repositories}")
+
+    if response.status != 200:
+        return []
+
+    return [
+        repository["original_url"].removesuffix(".git") for repository in repositories
+    ]
+
+
+async def migrate_to_gitea(
+    session: aiohttp.ClientSession,
+    clone_addr: str,
+    repo_name: str,
+    description: str,
+    base_url: str,
+    auth: aiohttp.BasicAuth,
+) -> List[str]:
+    log.debug("Origin " + clone_addr)
 
     response: aiohttp.ClientResponse = await session.post(
-        # url="http://127.0.0.1:13000/api/v1/repos/migrate",
-        # url="http://192.168.0.18:13000/api/v1/repos/migrate",
-        # auth=aiohttp.BasicAuth("root", "root"),
-        url="http://192.168.0.10:13000/api/v1/repos/migrate",
-        auth=aiohttp.BasicAuth("root", "123456"),
+        url=base_url + "/api/v1/repos/migrate",
+        auth=auth,
         json={
             "clone_addr": clone_addr,
             "repo_name": repo_name,
@@ -39,37 +89,34 @@ async def migrate_to_gitea(session, clone_addr, repo_name, description):
     log.debug(f"Body {await response.json()}")
 
 
+async def get_repos_blacklist():
+    async with aiohttp.ClientSession() as session:
+        base_urls = ["http://192.168.0.10:13000", "http://192.168.0.18:13000"]
+        auths = [aiohttp.BasicAuth("root", "123456"), aiohttp.BasicAuth("root", "root")]
+
+        for base_url, auth in zip(base_urls, auths):
+            organizations = await get_all_organizations_list(session, base_url, auth)
+            for organization in organizations:
+                repos_blacklist.add(
+                    await get_all_repositories_list(
+                        session, base_url, auth, organization
+                    )
+                )
+
+    return repos_blacklist
+
+
 async def migrate_to_gitea_from_github_trending():
+    await get_repos_blacklist()
+
     repositories = chain(
         await get_github_trending({"since": "daily"}),
-        # await get_github_trending({"since": "weekly"}),
-        # await get_github_trending({"since": "monthly"}),
-        await get_github_trending({"since": "daily"}, base_url + "/go"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/go"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/go"),
-        await get_github_trending({"since": "daily"}, base_url + "/python"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/python"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/python"),
-        # await get_github_trending({"since": "daily"}, base_url + "/kotlin"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/kotlin"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/kotlin"),
-        # await get_github_trending({"since": "daily"}, base_url + "/java"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/java"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/java"),
-        # await get_github_trending({"since": "daily"}, base_url + "/dart"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/dart"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/dart"),
-        # await get_github_trending({"since": "daily"}, base_url + "/c++"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/c++"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/c++"),
-        await get_github_trending({"since": "daily"}, base_url + "/rust"),
-        # await get_github_trending({"since": "weekly"}, base_url + "/rust"),
-        # await get_github_trending({"since": "monthly"}, base_url + "/rust"),
+        await get_github_trending({"since": "daily"}, GITHUB_TRENDING_URL + "/go"),
+        await get_github_trending({"since": "daily"}, GITHUB_TRENDING_URL + "/python"),
+        await get_github_trending({"since": "daily"}, GITHUB_TRENDING_URL + "/rust"),
     )
 
     async with aiohttp.ClientSession() as session:
-        # tasks = []
-
         for repository in repositories:
             try:
                 if repository["total_stars"] < 1500 or repository["forks"] < 500:
@@ -90,16 +137,3 @@ async def migrate_to_gitea_from_github_trending():
                 )
             except asyncio.exceptions.TimeoutError:
                 continue
-
-        #     tasks.append(
-        #         asyncio.create_task(
-        #             migrate_to_gitea(
-        #                 session,
-        #                 repository["url"],
-        #                 repository["repository"],
-        #                 repository["description"],
-        #             )
-        #         )
-        #     )
-
-        # await asyncio.wait(tasks)
--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -2,12 +2,21 @@
 Date: 2022.05.09 21:32
 Description: Omit
 LastEditors: Rustle Karl
-LastEditTime: 2022.05.09 21:32
+LastEditTime: 2022.05.25 10:23:11
 """
 import asyncio
 
-from github_trending_migrator.migrate import migrate_to_gitea_from_github_trending
+from github_trending_migrator.migrate import (
+    migrate_to_gitea_from_github_trending,
+    get_repos_blacklist,
+)
 
 
+# pytest tests/test_migrate.py::test_get_repos_blacklist -v -s
+def test_get_repos_blacklist():
+    assert len(get_repos_blacklist()) > 600
+
+
+# pytest tests/test_migrate.py::test_migrate_to_gitea_from_github_trending -v -s
 def test_migrate_to_gitea_from_github_trending():
     asyncio.run(migrate_to_gitea_from_github_trending())
